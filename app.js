41878d185e2878ef81e1837e0f074eb8f08be018
Ilknur Mustafov <ilknurilko@gmail.com>
2020-03-26 18:19:53 +0100

    Add MD5 Hash

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config()  // dotenv LVL3 Security
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+var md5 = require('md5');
 const app = express();
 
 app.use(express.static("public"));
@@ -22,8 +24,6 @@ const userSchema = new mongoose.Schema ( {
 });
 
 
-//  Encryption of password  LVL2 Security
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 // New User Model
 const User = new mongoose.model("User", userSchema)
@@ -43,7 +43,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){ // Encryption
     if(err){
@@ -56,7 +56,7 @@ app.post("/register", function(req, res) {
 // -----------------------------------------------
 app.post("/login", function(req, res) {
    const username = req.body.username;
-   const password = req.body.password;
+   const password = md5(req.body.password);
 
    User.findOne({email: username}, function(err, foundUser) { // Find= Decryption
      if(err){
